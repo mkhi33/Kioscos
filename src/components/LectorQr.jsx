92b756600bd1fa3db931366b5d6399f9d785c3cd
Amilcar Antonio Rodriguez Velasquez <aarodriguezv@unah.hn>
2022-04-14 11:53:56 -0600

    Se valida el dispositivo que se esta usando para capturar el código QR

--- src/components/LectorQr.jsx
+++ src/components/LectorQr.jsx
@@ -1,9 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { QrReader } from 'react-qr-reader';
 const LectorQr = () => {
 
     const [data, setData] = useState('No result');
     const [escaneando, setEscaneando] = useState(false);
+    const [facignMode, setFacignMode ] = useState(true);
+    const [constraints, setConstrains ] = useState({
+        facingMode: {
+            exact: 'environment'
+        }
+    });
+
+    useEffect( () => {
+        if( navigator.userAgent.match(/Android/i)
+            || navigator.userAgent.match(/webOS/i)
+            || navigator.userAgent.match(/iPhone/i)
+            || navigator.userAgent.match(/iPad/i)
+            || navigator.userAgent.match(/iPod/i)
+            || navigator.userAgent.match(/BlackBerry/i)
+            || navigator.userAgent.match(/Windows Phone/i)
+        ) {
+            setFacignMode(true)
+        }else setFacignMode(false)
+        
+    }, [])
+
 
 
 
@@ -17,6 +38,9 @@ const LectorQr = () => {
                         <div className='bg-white w-full md:w-full lg:w-1/4  ml-auto mr-auto h-auto mb-16 border rounded-2xl p-2'>
                             <QrReader
                                 className='border rounded-2xl'
+                                delay={300}
+                                constraints={ facignMode? constraints : null}
+     
                                 onResult={(result, error) => {
                                 if (!!result) {
                                     setData(result?.text);
